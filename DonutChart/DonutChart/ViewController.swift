f0136f93ba51117e79eb59447f52b227b0e7e0b0
Wojciech Charysz <wojciech.charysz@hmmh.de>
2016-03-15 13:37:43 +0100

    First commit

---
+++ DonutChart/DonutChart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DonutChart
+//
+//  Created by Wojciech Charysz on 15.03.16.
+//  Copyright © 2016 Wojciech Charysz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DonutChart/DonutChartTests/DonutChartTests.swift
@@ -0,0 +1,36 @@
+//
+//  DonutChartTests.swift
+//  DonutChartTests
+//
+//  Created by Wojciech Charysz on 15.03.16.
+//  Copyright © 2016 Wojciech Charysz. All rights reserved.
+//
+
+import XCTest
+@testable import DonutChart
+
+class DonutChartTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
